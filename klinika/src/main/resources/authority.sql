d3354b4cec3b14f00b478ce5803687f6cf369039
Milan <millan.djuric@hotmail.com>
2020-04-23 10:57:45 +0200

    role u sql fajlu

---
+++ klinika/src/main/resources/authority.sql
@@ -0,0 +1,5 @@
+insert into authority values(1, 'pacijent');
+insert into authority values(2, 'admin_klinickog_centra');
+insert into authority values(3, 'admin_klinike');
+insert into authority values(4, 'lekar');
+insert into authority values(5, 'medicinska_sestra');
