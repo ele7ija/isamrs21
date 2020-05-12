8debe975f28a3b63ca84ec21d6e54dc2a1ab5b6b
bpoprzen <bpoprzen@gmail.com>
2020-05-12 18:29:24 +0200

    Mala ispravka neimportovanog exceptiona

--- klinika/src/main/java/isamrs/tim21/klinika/services/UpitZaPregledeService.java
+++ klinika/src/main/java/isamrs/tim21/klinika/services/UpitZaPregledeService.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
